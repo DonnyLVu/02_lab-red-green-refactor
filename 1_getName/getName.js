56a283f69031017499486146d99602761ac7fafc
DonnyLVu <donnyvu897@gmail.com>
2020-12-01 13:52:08 -0800

    add passing getName with dot notation

---
+++ 1_getName/getName.js
@@ -0,0 +1,8 @@
+const getName = item => {
+  const name = item.name;
+  return name
+}
+const
+  module.exports = {
+    getName
+  };
---
+++ 1_getName/getName.test.js
@@ -0,0 +1,11 @@
+const { getName } = require('./getName.js')
+
+const spot = { name: 'spot', age: 5, weight: '20 lbs' }
+
+describe('getName', () => {
+  it('should take in an object and return the name correctly', () => {
+    const name = getName(spot);
+    expect(name)
+      .toEqual('spot')
+  });
+});
