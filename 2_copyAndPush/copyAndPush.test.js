2ef21f4cc8b04454844955f5437fc89d398be09e
DonnyLVu <donnyvu897@gmail.com>
2020-12-01 14:12:05 -0800

    add passing push

---
+++ 2_copyAndPush/copyAndPush.test.js
@@ -0,0 +1,11 @@
+const { copyAndPush } = require('./copyAndPush')
+
+const numbers = [1, 2, 3]
+
+describe('copyAndPush', () => {
+  it('returns a new array with all the items in the original array and a new item pushed to the end', () => {
+    const newArr = copyAndPush(numbers, 4);
+    expect(newArr)
+      .toEqual([1, 2, 3, 4])
+  });
+}); 
